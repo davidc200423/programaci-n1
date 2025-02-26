a4a85ff0a40803868cc7b4b02a3ae0d3229e7d0d
davidc200423 <davidc200423@gmail.com>
2025-02-26 15:24:23 -0500

    tranformacion de variables

---
+++ variable.py
@@ -0,0 +1,10 @@
+entero= 100
+print(type(entero))
+decimal = 10.3
+print(type(decimal))
+letra = "a"
+print (type(letra))
+mensaje = "hola"
+print(type(mensaje))
+estado = False
+print(type(estado))
